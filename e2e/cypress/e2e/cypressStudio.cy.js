68a4a31441d82f8065cb1171a2f9eb87fc7345dc
Eugene <eu.work@icloud.com>
2024-05-21 17:03:54 +0300

    update

--- e2e/cypress/e2e/cypressStudio.cy.js
+++ e2e/cypress/e2e/cypressStudio.cy.js
@@ -9,31 +9,23 @@ describe('Cypress Studio Demo', () => {
         cy.saveLocalStorageCache();
     });
 
-    /* ==== Test Created with Cypress Studio ==== */
     it('authInvalid', function () {
-        /* ==== Generated with Cypress Studio ==== */
         cy.visit('/');
         cy.get('#email_input').type('admin@mail.ru');
         cy.get('#password_input').type('Password1');
         cy.get('#login_button').click();
         cy.get('.toast_container').first().should("contain.text", "Ошибка");
-        /* ==== End Cypress Studio ==== */
     });
 
-    /* ==== Test Created with Cypress Studio ==== */
     it('authValid', function () {
-        /* ==== Generated with Cypress Studio ==== */
         cy.visit('/');
         cy.get('#email_input').type(Cypress.env("ADMIN_EMAIL"));
         cy.get('#password_input').type(Cypress.env("ADMIN_PASSWORD"));
         cy.get('#login_button').click();
         cy.get('.toast_container').first().should("contain.text", "Вы авторизовались");
-        /* ==== End Cypress Studio ==== */
     });
 
-    /* ==== Test Created with Cypress Studio ==== */
     it('districtCreate', function () {
-        /* ==== Generated with Cypress Studio ==== */
         cy.visit('/districts');
         cy.get('#add_district_button').click();
         cy.get('#select_administrative_district', {timeout: 10000}).click();
@@ -43,12 +35,9 @@ describe('Cypress Studio Demo', () => {
         cy.get('.drag_and_drop_area').first().selectFile("cypress/fixtures/img.png", {action: "drag-drop"})
         cy.get('#create_district_button').click();
         cy.get('.toast_container').first().should('contain.text', 'Район добавлен');
-        /* ==== End Cypress Studio ==== */
     });
 
-    /* ==== Test Created with Cypress Studio ==== */
     it('departmentCreate', function () {
-        /* ==== Generated with Cypress Studio ==== */
         cy.visit('/departments');
         cy.get('#add_department_button').click();
         cy.get('#added_district_select').click();
@@ -56,12 +45,9 @@ describe('Cypress Studio Demo', () => {
         cy.get('#department_name').type("department_name");
         cy.get('#create_department_button').click();
         cy.get('.toast_container').first().should('contain.text', 'Отдел добавлен');
-        /* ==== End Cypress Studio ==== */
     });
 
-    /* ==== Test Created with Cypress Studio ==== */
     it('themeCreate', function () {
-        /* ==== Generated with Cypress Studio ==== */
         cy.visit('/themes');
         cy.get('#add_theme_button').click();
         cy.get('#select_department_button').click();
@@ -69,12 +55,9 @@ describe('Cypress Studio Demo', () => {
         cy.get('#theme_name').type("theme_name");
         cy.get('#create_theme').click();
         cy.get('.toast_container').first().should('contain.text', 'Тематика добавлена');
-        /* ==== End Cypress Studio ==== */
     });
 
-    /* ==== Test Created with Cypress Studio ==== */
     it('userEdit', function () {
-        /* ==== Generated with Cypress Studio ==== */
         cy.visit('/users');
         cy.get('.user_row_actions').first().click();
         cy.get('.user_edit_action').first().click();
@@ -82,12 +65,9 @@ describe('Cypress Studio Demo', () => {
         cy.get('.select_department_element', {timeout: 10000}).first().click();
         cy.get('#save_user').click();
         cy.get('.toast_container').first().should('contain.text', 'Пользователь обновлен');
-        /* ==== End Cypress Studio ==== */
     });
 
-    /* ==== Test Created with Cypress Studio ==== */
     it('requestCreate', function () {
-        /* ==== Generated with Cypress Studio ==== */
         cy.visit('/requests/new');
         cy.get('#theme_select_button').click();
         cy.get('.theme_select_element').first().click();
@@ -103,16 +83,13 @@ describe('Cypress Studio Demo', () => {
         cy.get(".address_select_element", {timeout: 20000}).first().click()
         cy.get('#create_request_button').click();
         cy.get('.toast_container').first().should("contain.text", "создано");
-        /* ==== End Cypress Studio ==== */
     });
 
     it('requestTable', function () {
-        /* ==== Generated with Cypress Studio ==== */
         cy.visit('/requests');
         cy.get(".theme_description").first().should("have.text", request.description)
         cy.get(".contact_name").first().should("have.text", request.name)
         cy.get(".contact_email_url").first().should("have.attr", "href", `mailto:${request.email}`)
         cy.get(".contact_phone_url").first().should("have.attr", "href", `tel:+${request.phone}`)
-        /* ==== End Cypress Studio ==== */
     });
 });
